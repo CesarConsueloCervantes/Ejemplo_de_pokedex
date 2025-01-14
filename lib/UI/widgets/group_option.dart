d54b408284fef0de4a1524f123c4e7dcd3d79496
Cesario54(230017) <cesar.consuelo@cceo.com.mx>
2025-01-14 10:54:51 -0600

    New feature (group_option.dart): :sparkles: new container

--- lib/UI/widgets/group_option.dart
+++ lib/UI/widgets/group_option.dart
@@ -12,6 +12,16 @@ class GroupOption extends StatelessWidget {
           margin: EdgeInsets.symmetric(horizontal: 10),
           child: Text('Group', style: Theme.of(context).textTheme.headlineMedium, )
         ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 10),
+          width: double.infinity,
+          height: 70,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1),
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
       ],
     );
   }
