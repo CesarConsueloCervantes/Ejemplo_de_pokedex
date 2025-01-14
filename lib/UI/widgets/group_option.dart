5aa74034116af7f3c57c5fac45bd54620912c99c
Cesario54(230017) <cesar.consuelo@cceo.com.mx>
2025-01-14 10:11:25 -0600

    New feature (widgets): :sparkles: new custom widget

---
+++ lib/UI/widgets/group_option.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GroupOption extends StatelessWidget {
+  const GroupOption({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
