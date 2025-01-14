a5bc34b281d32e5bbd8b4bda2d015b2190d50011
Cesario54(230017) <cesar.consuelo@cceo.com.mx>
2025-01-14 11:08:36 -0600

    New feature (models): :sparkles: new model

---
+++ lib/UI/models/menu_group_option.dart
@@ -0,0 +1,11 @@
+class MenuGroupOption{
+  final String name;
+  final List group; // TODO change this to List<somting>
+
+  MenuGroupOption(
+    {
+      required this.name,
+      required this.group
+    }
+  );
+}
