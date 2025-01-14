cdaac8543b39f0c791a52a14891457a12a795bd2
Cesario54(230017) <cesar.consuelo@cceo.com.mx>
2025-01-14 11:48:48 -0600

    Removin code (group_option.dart): :fire: is Remove the Container of ListView

--- lib/UI/widgets/group_option.dart
+++ lib/UI/widgets/group_option.dart
@@ -14,22 +14,12 @@ class GroupOption extends StatelessWidget {
           margin: EdgeInsets.symmetric(horizontal: 10),
           child: Text('Group', style: Theme.of(context).textTheme.headlineMedium, )
         ),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 10),
-          width: double.infinity,
-          height: 70,
-          decoration: BoxDecoration(
-            border: Border.all(width: 1),
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(10)
-          ),
-          child: ListView.separated(
-            itemBuilder: (context, index) => ListTile(
-              title: Text(menuGroupOption[index].name, style: Theme.of(context).textTheme.bodyMedium),
-            ), 
-            separatorBuilder: (_, __) => Divider(), 
-            itemCount: menuGroupOption.length
-          ),
+        ListView.separated(
+          itemBuilder: (context, index) => ListTile(
+            title: Text(menuGroupOption[index].name, style: Theme.of(context).textTheme.bodyMedium),
+          ), 
+          separatorBuilder: (_, __) => Divider(), 
+          itemCount: menuGroupOption.length
         ),
       ],
     );
