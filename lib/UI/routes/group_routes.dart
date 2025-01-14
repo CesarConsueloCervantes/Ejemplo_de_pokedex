6cf34d0910ae0cc35428b9e74de713564e26c917
Cesario54(230017) <cesar.consuelo@cceo.com.mx>
2025-01-14 11:20:33 -0600

    New feature (routes): :sparkles: new group of routes for the custom widget group_option.dart

---
+++ lib/UI/routes/group_routes.dart
@@ -0,0 +1,18 @@
+import 'package:ejemplo_de_pokedex/UI/models/menu_group_option.dart';
+
+class GroupRoutes {
+  static final menuGroupOption = <MenuGroupOption>[
+    MenuGroupOption(
+      name: 'grupo 1', 
+      group: []
+    ),
+    MenuGroupOption(
+      name: 'grupo 2', 
+      group: []
+    ),
+    MenuGroupOption(
+      name: 'grupo 3', 
+      group: []
+    ),
+  ];
+}
