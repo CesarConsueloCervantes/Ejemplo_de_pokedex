8b9e2e0a4bb18140c20a2f0ddbe5fb1dae8425aa
Cesario54(230017) <cesar.consuelo@cceo.com.mx>
2025-01-14 13:05:00 -0600

    New feature (group_option.dart): :sparkles: new Container with the Text and new onTap in the LisTile

--- lib/UI/widgets/group_option.dart
+++ lib/UI/widgets/group_option.dart
@@ -1,4 +1,5 @@
 import 'package:ejemplo_de_pokedex/UI/routes/group_routes.dart';
+import 'package:ejemplo_de_pokedex/UI/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 class GroupOption extends StatelessWidget {
@@ -12,11 +13,20 @@ class GroupOption extends StatelessWidget {
         margin: EdgeInsets.symmetric(horizontal: 10),
         child: ListView.builder(
           itemBuilder: (context, index) => ListTile(
-            title: Text(menuGroupOption[index].name, style: Theme.of(context).textTheme.bodyMedium),
-          ), 
+            title: Container(
+              padding: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+              height: 45,
+              decoration: BoxDecoration(
+                border: Border.all(width: 1),
+                borderRadius: BorderRadius.circular(10)
+              ),
+              child:Text(menuGroupOption[index].name, style: Theme.of(context).textTheme.bodyLarge),
+              ),
+              onTap: () => ResultsScreen(),
+            ),
           itemCount: menuGroupOption.length
+          ), 
         ),
-      ),
     );
   }
 }
