71a622d91b656d491c62bd1041510b8bd1df86f2
Cesario54(230017) <cesar.consuelo@cceo.com.mx>
2025-01-15 12:07:00 -0600

    Custom widget fix (group_option.dart): :bug: Explanded switch to Flexible and shrinkWrap true in the ListView

--- lib/UI/widgets/group_option.dart
+++ lib/UI/widgets/group_option.dart
@@ -8,25 +8,24 @@ class GroupOption extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final menuGroupOption = GroupRoutes.menuGroupOption;
-    return Expanded(
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 10),
-        child: ListView.builder(
-          itemBuilder: (context, index) => ListTile(
-            title: Container(
-              padding: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
-              height: 45,
-              decoration: BoxDecoration(
-                border: Border.all(width: 1),
-                borderRadius: BorderRadius.circular(10)
-              ),
-              child:Text(menuGroupOption[index].name, style: Theme.of(context).textTheme.bodyLarge),
-              ),
-              onTap: () => ResultsScreen(),
+    return Flexible(
+      fit: FlexFit.loose,
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemBuilder: (context, index) => ListTile(
+          title: Container(
+            padding: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+            height: 45,
+            decoration: BoxDecoration(
+              border: Border.all(width: 1),
+              borderRadius: BorderRadius.circular(10)
             ),
-          itemCount: menuGroupOption.length
-          ), 
-        ),
+            child:Text(menuGroupOption[index].name, style: Theme.of(context).textTheme.bodyLarge),
+            ),
+            onTap: () => ResultsScreen(),
+          ),
+        itemCount: menuGroupOption.length
+      ),
     );
   }
 }
