0df204625af20d4ed2dcfdd36363cdd0df722d66
Cesario54(230017) <up230017@alumnos.upa.edu.mx>
2025-01-17 10:43:19 -0600

    Refactorizacion del ListView (results_screen): :hammer: se extrajo el ListView y se introdujo en el result_card.dart

--- lib/UI/screens/results_screen.dart
+++ lib/UI/screens/results_screen.dart
@@ -1,9 +1,22 @@
+import 'package:ejemplo_de_pokedex/UI/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final int name = ModalRoute.of(context)!.settings.arguments as int; 
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Group $name'),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) => ListTile(
+          title: ResultCard(index: index),
+          onTap: () => Navigator.pushNamed(context, 'details'),
+        ),
+        itemCount: 40,
+      ),
+    );
   }
 }
