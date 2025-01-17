5236bcbc01b48d9731491684c6b82c709f30ca3b
Cesario54(230017) <up230017@alumnos.upa.edu.mx>
2025-01-17 11:48:44 -0600

    Documentation (widgets.dart): :books: export stats_card.dart

--- lib/UI/widgets/widgets.dart
+++ lib/UI/widgets/widgets.dart
@@ -1,2 +1,3 @@
+export 'package:ejemplo_de_pokedex/UI/widgets/stats_card.dart';
 export 'package:ejemplo_de_pokedex/UI/widgets/group_option.dart';
 export 'package:ejemplo_de_pokedex/UI/widgets/result_card.dart';
