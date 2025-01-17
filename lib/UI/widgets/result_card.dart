480074a887023409a74a570db2572f5d2e1821d6
Cesario54(230017) <up230017@alumnos.upa.edu.mx>
2025-01-17 11:06:50 -0600

    New format (result_card.dart): :sparkles: new image added into Container and new format for the text

--- lib/UI/widgets/result_card.dart
+++ lib/UI/widgets/result_card.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
 class ResultCard extends StatelessWidget {
-  const ResultCard({super.key});
+  final int index;
+  const ResultCard({super.key, required this.index});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.only(left: 10, bottom: 5 ,top: 5, right: 100),
+      height: 100,
+      decoration: BoxDecoration(
+        //color: Colors.lightBlueAccent, TODO poner una forma de colorear dependiendo el tipo(que bonito)
+        border: Border.all(width: 1),
+        image: DecorationImage(
+          alignment: Alignment.centerRight,
+          image: NetworkImage('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10277.png'),
+          fit: BoxFit.contain
+          ),
+        borderRadius: BorderRadius.circular(10)
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('N.° $index', style: Theme.of(context).textTheme.headlineSmall),
+          Text('option $index', style: Theme.of(context).textTheme.bodyLarge),
+        //Text('Fletchinderssssssssssssssssssssssssssssssssssssssssssssss $index', style: Theme.of(context).textTheme.bodyLarge),
+        ],
+      )
+    );
   }
 }
