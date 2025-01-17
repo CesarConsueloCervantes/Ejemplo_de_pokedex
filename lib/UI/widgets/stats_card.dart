fc2b26f2426c74656625ac1058f9a41cdadf269f
Cesario54(230017) <up230017@alumnos.upa.edu.mx>
2025-01-17 12:09:13 -0600

    New variables (stats_card.dart): :sparkles: statname, stat, color variables added

--- lib/UI/widgets/stats_card.dart
+++ lib/UI/widgets/stats_card.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 class StatsCard extends StatelessWidget {
-  const StatsCard({super.key});
+  final String statname;
+  final String stat;
+  final Color color;
+  const StatsCard({super.key,required this.statname, required this.stat, required this.color});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      decoration: BoxDecoration(
+        color: color,
+        
+      ),
     );
   }
 }
