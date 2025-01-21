05ccffba6df33e7070ee7216b96e13586b7c337c
Cesario54(230017) <up230017@alumnos.upa.edu.mx>
2025-01-20 23:08:05 -0600

    New container (move_card.dart): :sparkles: Container with Boxdecoration and Column added

--- lib/UI/widgets/move_card.dart
+++ lib/UI/widgets/move_card.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
 class MoveCard extends StatelessWidget {
-  const MoveCard({super.key});
+  final int index;
+  const MoveCard({super.key, required this.index});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      width: double.infinity,
+      height: 75, 
+      decoration: BoxDecoration(
+        border: Border.all(width: 1),
+        color: const Color.fromARGB(255, 234, 234, 234),
+        borderRadius: BorderRadius.circular(10)
+      ),
+      child: Column(
+        children: [
+        ],
+      ),
+    );
   }
 }
