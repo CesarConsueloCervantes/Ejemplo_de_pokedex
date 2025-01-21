a17c2d6d68b4d9d0a9e5ed7c6fe724576e6a0a21
Cesario54(230017) <up230017@alumnos.upa.edu.mx>
2025-01-20 21:40:02 -0600

    New custom widget (widgets): :sprakles: Movecard added

---
+++ lib/UI/widgets/move_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MoveCard extends StatelessWidget {
+  const MoveCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/UI/widgets/widgets.dart
+++ lib/UI/widgets/widgets.dart
@@ -1,3 +1,4 @@
+export 'package:ejemplo_de_pokedex/UI/widgets/move_card.dart';
 export 'package:ejemplo_de_pokedex/UI/widgets/stats_card.dart';
 export 'package:ejemplo_de_pokedex/UI/widgets/group_option.dart';
 export 'package:ejemplo_de_pokedex/UI/widgets/result_card.dart';
