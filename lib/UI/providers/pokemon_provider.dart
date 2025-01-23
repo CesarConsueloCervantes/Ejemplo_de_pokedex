2668ffef9435c64eb8c9d0e46a904e6225d4adc2
Cesarcceo <cesar.consuelo@cceo.com.mx>
2025-01-23 12:11:17 -0600

    New metod (pokemon_provider): :sparkles: the metod getPokemonOnDisplay added

--- lib/UI/providers/pokemon_provider.dart
+++ lib/UI/providers/pokemon_provider.dart
@@ -3,6 +3,10 @@ import 'package:http/http.dart' as http;
 
 class PokemonProvider {
     final String _baseUrl = 'https://pokeapi.co/api/v2';
+    final int _pageLimit = 40;
+
+  int page = 0; //de 40 en 40
+  List<Pokemon> pokemonsOnDisplay = [];
   
   Future<String> _getJsonData(String endpoint) async {
     final url = Uri.https(_baseUrl, endpoint);
@@ -11,4 +15,15 @@ class PokemonProvider {
     return response.body;
   }
 
+    Future<List<Pokemon>> getPokemonOnDisplay () async{
+    final jsonData = await _getJsonData('pokemon?offset=$page&limit=$_pageLimit');
+    final pokemonResponse = pokemonResponseFromJson(jsonData);
+    page += 40;
+
+    final List<NameUrl> nameUrlPokemons =pokemonResponse.results;
+    final List<Pokemon> pokemons ;
+
+    pokemonsOnDisplay=pokemons;
+    return pokemons;
+  }
 }
