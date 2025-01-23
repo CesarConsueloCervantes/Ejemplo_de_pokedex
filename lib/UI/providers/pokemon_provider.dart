e8cfcd1c849a384c5e84ebc79354726dc6274be7
Cesarcceo <cesar.consuelo@cceo.com.mx>
2025-01-23 12:09:20 -0600

    New metod (pokemon_provider): :sparkles: the metod _getJsonData added

--- lib/UI/providers/pokemon_provider.dart
+++ lib/UI/providers/pokemon_provider.dart
@@ -2,6 +2,13 @@ import 'package:ejemplo_de_pokedex/UI/models/models.dart';
 import 'package:http/http.dart' as http;
 
 class PokemonProvider {
+    final String _baseUrl = 'https://pokeapi.co/api/v2';
   
-  
+  Future<String> _getJsonData(String endpoint) async {
+    final url = Uri.https(_baseUrl, endpoint);
+
+    final response = await http.get(url);
+    return response.body;
+  }
+
 }
