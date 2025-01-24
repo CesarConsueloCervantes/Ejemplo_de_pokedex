fbcf45166b77ef99d47e771c6e1300a4de9ab9cd
Cesarcceo <cesar.consuelo@cceo.com.mx>
2025-01-24 12:30:58 -0600

    Remove variable (pokemon_provider.dart): :fire: Remove the variable page

--- lib/UI/providers/pokemon_provider.dart
+++ lib/UI/providers/pokemon_provider.dart
@@ -5,7 +5,6 @@ class PokemonProvider {
     final String _baseUrl = 'https://pokeapi.co/api/v2';
     final int _pageLimit = 40;
 
-  int page = 0; //de 40 en 40
   List<Pokemon> pokemonsOnDisplay = [];
   Map<String, List<PMove>> pokemonMoves = {};
   List<PMove> movesOnDisplay = [];
@@ -17,10 +16,9 @@ class PokemonProvider {
     return response.body;
   }
 
-    Future<List<Pokemon>> getPokemonOnDisplay () async{
+    Future<List<Pokemon>> getPokemonOnDisplay (int page) async{
     final jsonData = await _getJsonData('pokemon?offset=$page&limit=$_pageLimit');
     final pokemonResponse = pokemonResponseFromJson(jsonData);
-    page += 40;
 
     final List<NameUrl> nameUrlPokemons =pokemonResponse.results;
     final List<Pokemon> pokemons = await convertNameUrltoPokemons(nameUrlPokemons);
